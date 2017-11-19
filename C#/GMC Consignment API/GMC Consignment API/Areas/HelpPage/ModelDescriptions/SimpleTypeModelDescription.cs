aee6a98f5eebb84bbb0cd1f979593deaefe02ce3
Ethan Ollins <ethanollins6@gmail.com>
2017-11-19 11:28:03 -0800

    Initial commit

---
+++ C#/GMC Consignment API/GMC Consignment API/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace GMC_Consignment_API.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ C#/GMC Consignment API/GMC Consignment API/Areas/HelpPage/SampleGeneration/SampleDirection.cs
@@ -0,0 +1,11 @@
+namespace GMC_Consignment_API.Areas.HelpPage
+{
+    /// <summary>
+    /// Indicates whether the sample is used for request or response
+    /// </summary>
+    public enum SampleDirection
+    {
+        Request = 0,
+        Response
+    }
+}
